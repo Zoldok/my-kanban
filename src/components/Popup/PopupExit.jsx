e42ac34c3c7b8382315cbafa2ccc91df7ac31856
Zoldok <max_kyrtimov@mail.ru>
2026-09-23 23:50:20 +0300

    fix bags hw 1

--- src/components/Popup/PopupExit.jsx
+++ src/components/Popup/PopupExit.jsx
@@ -9,20 +9,25 @@ const PopupExit = ({ isOpen, onClose, onConfirm }) => {
   };
 
   return (
-    <Popup isOpen={isOpen} onClose={onClose} title='Выйти из аккаунта?'>
-      <div className='popup-exit__body'>
-        <div className='popup-exit__actions'>
+    <Popup
+      isOpen={isOpen}
+      onClose={onClose}
+      title="Выйти из аккаунта?"
+      className="popup-exet"
+    >
+      <div className="popup-exit__body">
+        <div className="popup-exit__actions">
           <button
-            className='popup-exit__yes _hover01'
+            className="popup-exit__yes _hover01"
             onClick={handleConfirm}
-            type='button'
+            type="button"
           >
             Да, выйти
           </button>
           <button
-            className='popup-exit__no _hover03'
+            className="popup-exit__no _hover03"
             onClick={onClose}
-            type='button'
+            type="button"
           >
             Нет, остаться
           </button>
